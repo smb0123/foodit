8d81011c8daba7cff25abd240dcc951b054a4fe2
minbo <smb5174@naver.com>
2024-02-06 23:42:22 +0900

    feat: 삭제 버튼 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
-import FoodList from "./FoodList";
-import items from "../mock.json";
 import { useState } from "react";
+import FoodList from "./FoodList";
+import mockItems from "../mock.json";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
 
   const handleNewestClick = () => setOrder("createdAt");
+
   const handleCalorieClick = () => setOrder("calorie");
+
+  const [items, setItems] = useState(mockItems);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <FoodList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
