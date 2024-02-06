f990625c2e61cf2645e8996fd125ee1d3f2dd079
minbo <smb5174@naver.com>
2024-02-06 18:57:42 +0900

    feat: 최신순 칼로리순 버튼 구현

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,5 +1,12 @@
+import "./FoodList.css";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
 function FoodListItem({ item }) {
-  const { imgUrl, title, calorie, content } = item;
+  const { imgUrl, title, calorie, content, createdAt } = item;
 
   return (
     <div className="FoodListItem">
@@ -7,6 +14,7 @@ function FoodListItem({ item }) {
       <div>{title}</div>
       <div>{calorie}</div>
       <div>{content}</div>
+      <div>{formatDate(createdAt)}</div>
     </div>
   );
 }
