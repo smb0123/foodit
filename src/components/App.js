7d9549acdcaaaf7e0910a66a11dff85ba08878d6
minbo <smb5174@naver.com>
2024-02-07 11:32:34 +0900

    feat: page load 시 데이터 불러오기

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import FoodList from "./FoodList";
+import { useEffect, useState } from "react";
 import { getFoods } from "../api";
+import FoodList from "./FoodList";
 
 function App() {
   const [order, setOrder] = useState("createdAt");
@@ -15,20 +15,22 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
-    // 여기에 코드를 작성하세요
+  const handleLoad = async () => {
     const { foods } = await getFoods();
     setItems(foods);
   };
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
+  useEffect(() => {
+    handleLoad();
+  }, []);
+
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
