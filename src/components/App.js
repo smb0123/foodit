ab4ae3c6052318b0f51e86aafe014c906625a9c6
minbo <smb5174@naver.com>
2024-02-07 17:02:58 +0900

    feat: 검색 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ function App() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [loadingError, setLoadingError] = useState(null);
+  const [search, setSearch] = useState("");
 
   const handleNewestClick = () => setOrder("createdAt");
 
@@ -30,7 +31,6 @@ function App() {
     } finally {
       setIsLoading(false);
     }
-
     const {
       foods,
       paging: { nextCursor },
@@ -47,7 +47,13 @@ function App() {
     handleLoad({
       order,
       cursor,
+      search,
     });
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target["search"].value);
   };
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
@@ -55,13 +61,18 @@ function App() {
   useEffect(() => {
     handleLoad({
       order,
+      search,
     });
-  }, [order]);
+  }, [order, search]);
 
   return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
+      <form onSubmit={handleSearchSubmit}>
+        <input name="search" />
+        <button type="submit">검색</button>
+      </form>
       <FoodList items={sortedItems} onDelete={handleDelete} />
       {cursor && (
         <button disabled={isLoading} onClick={handleLoadMore}>
